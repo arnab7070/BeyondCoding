d6640b2a721f34e456fb33126f94a439418a9191
Arnab Nandi <arnabnandi7074@gmail.com>
2022-08-11 19:44:41 +0530

    Initial Commit

--- C++ Programs/AOT DSA LAB/Day3/infixToPostfix.cpp
+++ C++ Programs/AOT DSA LAB/Day3/infixToPostfix.cpp
@@ -1,63 +1,58 @@
 #include<iostream>
-#include<math.h>
+#include<string>
 #include<stack>
 using namespace std;
-int precedence(char c){
-    if (c=='^')
-    {
+
+int precedence(char a){
+    if(a=='^'){
         return 3;
     }
-    else if (c=='*'||c=='/')
-    {
+    else if(a=='*'||a=='/'){
         return 2;
     }
-    else if(c=='+'||c=='-'){
+    else if(a=='+'||a=='-'){
         return 1;
     }
-    else{
-        return -1;
-    }
+
+    return -1;
 }
-string infixToPostfix(string s){
+string infixToPostfix(string str){
+
+    string ans;
     stack<char> st;
-    string result;
-    for(int i = 0; i < s.length(); i++){
-        if(s[i]>='a'&&s[i]<='z'||s[i]>='A'&&s[i]<='Z'){
-            result+=s[i];    
+    for(int i = 0; i < str.length(); i++){
+        if((str[i]>='a'&&str[i]<='z')||(str[i]>='A'&&str[i]<='Z')){
+            ans+=str[i];
         }
-        else if(s[i]=='('){
-            st.push(s[i]);
+        else if(str[i]=='('){
+            st.push(str[i]);
         }
-        else if(s[i]==')'){
-            while(!st.empty()&&st.top()!='('){
-                result+=st.top();
+        else if(str[i]==')'){
+            while(!st.empty() && st.top()!='('){
+                ans += st.top();
                 st.pop();
             }
-            if (!st.empty())
-            {
+            if(!st.empty()){
                 st.pop();
-            } 
+            }    
         }
         else{
-            while (!st.empty()&&precedence(st.top())>precedence(s[i]))
-            {
-                result+=st.top();
+            while(!st.empty() && (precedence(st.top())>precedence(str[i]))){
+                ans += st.top();
                 st.pop();
-            } 
-            st.push(s[i]);
+            }     
+            st.push(str[i]);
         }
     }
-    while (!st.empty())
-    {
-        result+=st.top();
+    while(!st.empty()){
+        ans += st.top();
         st.pop();
     }
-    
-    return result;
+    return ans;
 }
-int main(){
-
-   cout<<infixToPostfix("(a-b/c)*(a/k-l)")<<endl;
-
-return 0;
+int main()
+{
+    string equation = "(a+b*c)+(a-b/c)";
+    cout<<infixToPostfix(equation)<<endl;
+    return 0;
 }
