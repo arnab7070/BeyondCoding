83ef769fdea6d49fe10f138ae2512d052a4181db
arnab7070 <arnabnandi7074@gmail.com>
2022-08-30 18:56:25 +0530

    Initial Commit

--- C++ Programs/AOT DSA LAB/Day5/TowerOfHanoi.cpp
+++ C++ Programs/AOT DSA LAB/Day5/TowerOfHanoi.cpp
@@ -1,3 +1,19 @@
+/*
+START
+Procedure Hanoi(disk, source, dest, aux)
+
+   IF disk == 1, THEN
+      move disk from source to dest             
+   ELSE
+      Hanoi(disk - 1, source, aux, dest)     // Step 1
+      move disk from source to dest          // Step 2
+      Hanoi(disk - 1, aux, dest, source)     // Step 3
+   END IF
+   
+END Procedure
+STOP
+*/
+
 #include<iostream>
 using namespace std;
 void TOH(int n, char src, char aux, char dest){
