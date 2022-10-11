f8f98b68299529638ee8a125d44c05aad5c7bba9
Arpan De <96812019+neo-deus@users.noreply.github.com>
2022-10-11 16:29:37 +0530

    Added a C file

---
+++ C Programs/Practice Problems/a1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main()
+{
+  int a;
+ printf("enter a no. :");
+ scanf("%d",&a);
+ if(a%2==0)
+ printf("the no. is even");
+ else
+ printf("the no. is odd");
+ return 0;
+}
